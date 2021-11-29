a2670fc89afc2ede53f3ac63bcd8da0a0175fbaf
Poke Code <90790144+aucoats@users.noreply.github.com>
2021-11-29 13:20:01 -0500

    generate HTML

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
+
+const profileDataArgs = process.argv.slice(2);
+// console.log(profileDataArgs);
+
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+// ^^^ this can also be done like this: 
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+})
 
 // don't need parenthesis for one argument arrow function syntax
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
 
-    console.log('======================');
+//     console.log('======================');
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//     // Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
