8e75af0d160438891b3ba22e213c7b315914c5e3
Poke Code <90790144+aucoats@users.noreply.github.com>
2021-11-29 18:44:56 -0500

    inquirer

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
-// console.log(profileDataArgs);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers))
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js')
 
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-// ^^^ this can also be done like this: 
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// // replaced with the above ^ bc of inquirer package
+// // const profileDataArgs = process.argv.slice(2);
+// // console.log(profileDataArgs);
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// // const name = profileDataArgs[0];
+// // const github = profileDataArgs[1];
+// // ^^^ this can also be done like this: 
 
-    console.log('Portfolio complete! Check out index.html to see the output!')
-})
+// // const [name, github] = profileDataArgs;
+
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// })
 
 // don't need parenthesis for one argument arrow function syntax
 // const printProfileData = profileDataArr => {
