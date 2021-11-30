cc0b07cd295ef112aff1d7181afbbb9288bd1d25
Poke Code <90790144+aucoats@users.noreply.github.com>
2021-11-30 14:55:39 -0500

    validation

--- app.js
+++ app.js
@@ -5,18 +5,34 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!')
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
-          },
-          {
+            message: 'Enter your GitHub Username (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub Username!')
+                    return false;
+                }
+          }
+        },
+        {
             type: 'input',
             name: 'about',
             message: 'Provide some information about yourself:'
-          }
+        }
     ])  
 }
 
@@ -35,13 +51,29 @@ const promptProject = portfolioData => {
             {
                 type: 'input', 
                 name: 'name', 
-                message: 'What is the name of your project?'
-            }, 
+                message: 'What is the name of your project? (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your project name!')
+                        return false;
+                    }
+                }, 
+            },
             {
                 type: 'input',
                 name: 'description',
-                message: 'Provide a description of the project (Required)'
-              },
+                message: 'Provide a description of the project (Required)',
+                validate: descriptionInput => {
+                    if (descriptionInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a description!')
+                        return false;
+                    }
+                },
+            },
               {
                 type: 'checkbox',
                 name: 'languages',
@@ -51,8 +83,16 @@ const promptProject = portfolioData => {
               {
                 type: 'input',
                 name: 'link',
-                message: 'Enter the GitHub link to your project. (Required)'
-              },
+                message: 'Enter the GitHub link to your project. (Required)',
+                validate: linkInput => {
+                    if (linkInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your link!')
+                        return false;
+                    }
+                    },
+                },
               {
                 type: 'confirm',
                 name: 'feature',
