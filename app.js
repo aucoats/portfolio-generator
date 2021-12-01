5f7a94655c64bed09b3024177479613fa68d6f7f
Poke Code <90790144+aucoats@users.noreply.github.com>
2021-12-01 13:56:48 -0500

    refactoring

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template.js')
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const mockData = {
     name: 'Lernantino',
@@ -182,20 +182,47 @@ const promptProject = portfolioData => {
 //     .then(
         // portfolioData => {
         // const pageHTML = generatePage(portfolioData);
-        const pageHTML = generatePage(mockData);
+        // const pageHTML = generatePage(mockData);
         
 
-        fs.writeFile('index.html', pageHTML, err => {
-            if (err) throw new Error(err); 
+        // fs.writeFile('./dist/index.html', pageHTML, err => {
+        //     if (err) throw new Error(err); 
         
-            console.log('Portfolio complete! Check out index.html to see the output!')
-        })
+        //     console.log('Portfolio complete! Check out index.html to see the output!')
+        
+        //     fs.copyFile('./src/style.css', './dist/style.css', err => {
+        //         if (err) {
+        //           console.log(err);
+        //           return;
+        //         }
+        //         console.log('Style sheet copied successfully!');
+        //       });
+        // })
     // });
 
+    // refactored to look like this: 
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 
 
-// // replaced with the above ^ bc of inquirer package
+
 // // const profileDataArgs = process.argv.slice(2);
 // // console.log(profileDataArgs);
 
